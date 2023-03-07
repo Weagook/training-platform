273c716f1819eee11d81e49d68d9d6c70d5d047c
Weagook <weagook@gmail.com>
2023-03-07 15:58:24 +0600

    initial models django

--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 def indexMain(request):
+    print(request.GET)
     return HttpResponse('Главная страница сайта')
+
+
+def indexLevel(request, lvlid):
+    return HttpResponse(f'<h1>Уровень номер {lvlid}</h1>')
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена. Ошибка 404.</h1>')
--- nodeCode/urls.py
+++ nodeCode/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from main.views import pageNotFound
+from nodeCode import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = pageNotFound
